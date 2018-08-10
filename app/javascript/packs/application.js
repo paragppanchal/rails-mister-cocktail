179b819acd814cd1c22f9bf9a5d7311efeb46d5c
Parag Panchal <parag@relteksys.com.au>
2018-08-10 13:39:26 +1000

    deploy to heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-import 'bootsnap';
+import 'bootstrap';
+
+//'bootsap'
 
 console.log('Hello World from Webpacker')
